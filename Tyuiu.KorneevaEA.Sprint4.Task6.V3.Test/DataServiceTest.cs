8775315f48a423dea49b11374a70b5f8fbf1f636
Alluka Zollyck <144251274+KorneevaElizaveta@users.noreply.github.com>
2023-11-12 15:05:14 +0500

    Task6

---
+++ Tyuiu.KorneevaEA.Sprint4.Task6.V3.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.KorneevaEA.Sprint4.Task6.V3.Lib;
+
+namespace Tyuiu.KorneevaEA.Sprint4.Task6.V3.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalc()
+        {
+            DataService ds = new DataService();
+            var month = new string[] { "Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь", };
+            int res = ds.Calculate(month);
+            int wait = 4;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.KorneevaEA.Sprint4.Task6.V3.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KorneevaEA.Sprint4.Task6.V3.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KorneevaEA.Sprint4.Task6.V3.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4805e9db-5f2e-4c36-b00a-12aeb07293aa")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
